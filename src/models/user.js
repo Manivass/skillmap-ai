f771c56e596574279c67504967c155d50e3f4091
Manivass <manivassv@gmail.com>
2026-03-17 11:24:16 +0530

    added schemas in user model

--- src/models/user.js
+++ src/models/user.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       maxLength: 12,
       trim: true,
     },
+    userName: {
+      type: String,
+      maxLength: 14,
+      trim: true,
+    },
     emailId: {
       type: String,
       required: true,
@@ -28,6 +33,7 @@ const userSchema = new mongoose.Schema(
         }
       },
     },
+
     password: {
       type: String,
     },
@@ -38,6 +44,59 @@ const userSchema = new mongoose.Schema(
         message: `{VALUE} is not valid provider `,
       },
       default: "local",
+    },
+    profileURL: {
+      type: String,
+    },
+    contactEmail: {
+      type: String,
+      validate: function (value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("email is not valid");
+        }
+      },
+    },
+    contactPhoneNo: {
+      type: Number,
+      min: 1000000000,
+      max: 9999999999,
+    },
+    country: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    skills: {
+      type: [String],
+      maxLength: 10,
+    },
+    githubURL: {
+      type: String,
+      validate: function (value) {
+        if (!validator.isURL(value)) {
+          throw new Error("github url is not valid");
+        }
+      },
+    },
+    leetcodeURL: {
+      type: String,
+      validate: function (value) {
+        if (!validator.isURL(value)) {
+          throw new Error("leetcode url is not valid");
+        }
+      },
+    },
+    instagramURL: {
+      type: String,
+      validate: function (value) {
+        if (!validator.isURL(value)) {
+          throw new Error("instagram url is not valid");
+        }
+      },
     },
   },
   {
